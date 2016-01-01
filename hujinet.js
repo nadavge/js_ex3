b095a3c785b1e26721f101506076ef61e8df17a7
Nadav Geva <nadavgeva4@gmail.com>
2016-01-01 18:53:45 +0200

    Change tabs for spaces

--- hujinet.js
+++ hujinet.js
@@ -1,12 +1,16 @@
+var net = require('net');
+
 module.exports.connectionHandler = function(server) {
 
-	this.onConnection = function(connection) {
+    this.onConnection = function(connection) {
+        c.on('data', function(data) {
+            console.log(data.toString('ascii', 0, data.length));
+        });
+    }
 
-	}
-
-	this.handleRequest = function(???) {
-		response = compose(13, Body);
-		response.pipe(connection);
-	}
+    this.handleRequest = function() {
+        response = compose(13, Body);
+        response.pipe(connection);
+    }
 }
 
--- hujiwebserver.js
+++ hujiwebserver.js
@@ -2,22 +2,22 @@ var hujinet = require('./hujinet');
 var net = require('net');
 
 module.exports.start = function(port, rootFolder, errorCallback) {
-	return new serverObj(port, rootFolder, errorCallback);
+    return new serverObj(port, rootFolder, errorCallback);
 }
 
 function serverObj(port, rootFolder, errorCallback) {
-	var server;
-	var connectionHandler;
+    var server;
+    var connectionHandler;
 
-	Object.defineProperty(this, 'port', {
-		value: port
-	});
-	Object.defineProperty(this, 'rootFolder', {
-		value: rootFolder
-	});
+    Object.defineProperty(this, 'port', {
+        value: port
+    });
+    Object.defineProperty(this, 'rootFolder', {
+        value: rootFolder
+    });
 
-	connectionHandler = new hujinet.connectionHandler(this);
-	server = net.createServer(connectionHandler.onConnection);
-	server.on('error', errorCallback);
-	server.listen(port, errorCallback);
+    connectionHandler = new hujinet.connectionHandler(this);
+    server = net.createServer(connectionHandler.onConnection);
+    server.on('error', errorCallback);
+    server.listen(port, errorCallback);
 }
