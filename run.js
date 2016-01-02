8cc55b3783316b146778612c1b74f7d81b6bdfc4
Nadav Geva <nadavgeva4@gmail.com>
2016-01-02 20:07:24 +0200

    Set run.js to stop the server

--- run.js
+++ run.js
@@ -1,5 +1,15 @@
 var hujiserver = require('./hujiwebserver');
 
-var server = hujiserver.start(80, '/tmp/potato', function(e) {
-	e ? console.log(e) : console.log('Server is up');
+var server = hujiserver.start(8000, '/tmp/potato', function(e) {
+	if(e) {
+		console.log(e)
+	} else {
+		console.log('Server is up');
+		setTimeout(function() {
+			server.stop(function() {
+				console.log('Server stopped');
+			});
+		}, 5000);
+	}
 });
+
