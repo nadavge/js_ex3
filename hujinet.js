c8b91ea4f77c972a04189f9be6757cc36e8837c8
Nadav Geva <nadavgeva4@gmail.com>
2016-01-02 22:55:48 +0200

    Add simple request checking

--- hujinet.js
+++ hujinet.js
@@ -1,9 +1,14 @@
 var net = require('net');
 var requestparser = require('./hujirequestparser');
+var path = require('path');
+var fs = require('fs');
 
 module.exports.connectionHandler = function(server) {
+    var myServer = server;
+    var that = this;
 
     this.onConnection = function(conn) {
+        console.log('Connection!'); //TODO remove
         conn.on(
             'data',
             function(data) {
@@ -11,18 +16,43 @@ module.exports.connectionHandler = function(server) {
                 // TODO maybe handle the data differently, since this is
                 // a stream, we might get it by chunks.. Need to check
                 data = data.toString('ascii', 0, data.length)
-
+                console.log('Incoming request:\n\n' + data);
                 try {
                     request = requestparser.parse(data);
-                    this.handleRequest(request);
+                    console.log(request);
+                    that.handleRequest(conn, request);
                 } catch (e) {
+                    console.log(e);
                     // TODO handle exception
                 }
             });
     }
 
-    this.handleRequest = function(request) {
+    this.handleRequest = function(conn, request) {
+        var filepath;
+
+        if (! path.isAbsolute(request.uri)) {
+            //TODO handle access denied / 404
+            console.log('File is going to be out');
+        }
+
+        filepath = path.join(myServer.rootFolder, request.uri);
+        fs.stat(filepath, function(err, stats) {
+            if (err) {
+                // TODO return 404
+                console.log('File isnt available');
+                return;
+            }
+            if (! stats.isFile()) {
+                console.log('Isn\'t file');
+                return;
+            }
+
+            console.log(stats.size);
+        });
         //response.pipe(connection);
+
+        // Close connection of necessary
     }
 }
 
