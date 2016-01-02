80cee4866cec7ca84aa2d12df9badc6242d2eb4d
Or Latovitz <orlatovitz@gmail.com>
2016-01-03 01:56:02 +0200

    wrote load again using test

--- load.js
+++ load.js
@@ -2,31 +2,31 @@ var serverObj;
 var httpModule = require('http');
 var hujiServer = require('./hujiwebserver');
 var servPort = 1337;
-var filePath = '/index.html';
+//var filePath = '/index.html';
 var rootFolder = './ex2';
-var fileUri = 'http://localhost:' + servPort + filePath;
+//var fileUri = 'http://localhost:' + servPort + filePath;
 var NUM_OF_CONCURRENT = 1;
 var SUCCESS_STATUS_CODE = 200;
 
-function errorHandler(getError)
-{
-    console.log('Woops! something happend: ' + getError.message);
-}
-
-function getHandler(getResult)
-{
-    if(getResult.statusCode != SUCCESS_STATUS_CODE)
-    {
-        console.log('A request has gone bad! Error Code:' + getResult.statusCode);
-    }
-
-    else
-    {
-        console.log('requested successfully!');
-    }
-
-    getResult.resume();
-}
+//function errorHandler(getError)
+//{
+//    console.log('Woops! something happend: ' + getError.message);
+//}
+//
+//function getHandler(getResult)
+//{
+//    if(getResult.statusCode != SUCCESS_STATUS_CODE)
+//    {
+//        console.log('A request has gone bad! Error Code:' + getResult.statusCode);
+//    }
+//
+//    else
+//    {
+//        console.log('requested successfully!');
+//    }
+//
+//    getResult.resume();
+//}
 
 serverObj = hujiServer.start(servPort, rootFolder, function(e){
 
@@ -44,14 +44,27 @@ serverObj = hujiServer.start(servPort, rootFolder, function(e){
 
 function runTests()
 {
-    var tempGetResponse;
+    //var tempGetResponse;
 
     for(var i = 0; i < NUM_OF_CONCURRENT; i++)
     {
         console.log('Starting request num: ' + (i+1));
-        tempGetResponse = httpModule.get(fileUri, getHandler).on('error',errorHandler);
+        //tempGetResponse = httpModule.get(fileUri, getHandler).on('error',errorHandler);
+        testOk();
     }
 
     console.log("Finished Load Testing!");
 
+}
+
+function testOk() {
+    httpModule.get({
+            port: servPort,
+            path: '/index.html'
+        },
+        function (response) {
+            console.log('testOk result:');
+            console.log((response.statusCode === SUCCESS_STATUS_CODE) ? 'Success' : 'Failed');
+        }
+    );
 }
