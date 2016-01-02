db242367f656a0afd28b7b121930cf3d0b5dc3ab
Nadav Geva <nadavgeva4@gmail.com>
2016-01-02 20:06:56 +0200

    Implement basic data handler

--- hujinet.js
+++ hujinet.js
@@ -1,16 +1,28 @@
 var net = require('net');
+var requestparser = require('./hujirequestparser');
 
 module.exports.connectionHandler = function(server) {
 
-    this.onConnection = function(connection) {
-        c.on('data', function(data) {
-            console.log(data.toString('ascii', 0, data.length));
-        });
+    this.onConnection = function(conn) {
+        conn.on(
+            'data',
+            function(data) {
+                var request;
+                // TODO maybe handle the data differently, since this is
+                // a stream, we might get it by chunks.. Need to check
+                data = data.toString('ascii', 0, data.length)
+
+                try {
+                    request = requestparser.parse(data);
+                    this.handleRequest(request);
+                } catch (e) {
+                    // TODO handle exception
+                }
+            });
     }
 
-    this.handleRequest = function() {
-        response = compose(13, Body);
-        response.pipe(connection);
+    this.handleRequest = function(request) {
+        //response.pipe(connection);
     }
 }
 
